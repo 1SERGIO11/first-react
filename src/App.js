622b9e8346a1cac62f538d7c0026c6c5fb1bb85e
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-07-18 15:38:58 +0500

    Добавлены стили css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header></Header>
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'></img>
+        <div>Ava + description</div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
