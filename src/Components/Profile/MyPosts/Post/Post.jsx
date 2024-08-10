e1351f5124a757e20ad16e2e37469364883b08cd
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-10 16:25:15 +0500

    Добавил новые компоненты для постов

---
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={classes.item}>
+      <img src="https://avatars.mds.yandex.net/get-shedevrum/11511289/cc2855ebcbb411eea6ebbaaee90618f0/orig" alt="" />
+      {props.message}
+      <div>
+        <span>like</span>
+      </div>
+    </div>
+  )
+}
+export default Post;
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
 import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (<div className={classes.content}>
     <img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'></img>
     <div>Ava + description</div>
-    <div>
-      My posts
-      <div>
-        New post
-      </div>
-      <div className='posts'>
-        <div className={classes.item}>
-          Post 1
-        </div>
-        <div className={classes.item}>
-          Post 2
-        </div>
-      </div>
-    </div>
+    <MyPosts/>
   </div>)
 }
 export default Profile;
