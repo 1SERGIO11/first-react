98c8cbe427dfaa73b9b3335775741a28a36ca922
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-11 12:25:57 +0500

    Добавил базовую структуру диалогов

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,8 +4,32 @@ import classes from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return(
-        <div className={classes.content}>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + " " + classes.active}>
+                    Sergey
+                </div>
+                <div className={classes.dialog}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Marina
+                </div>
+                <div className={classes.dialog}>
+                    Artyom
+                </div>
+                <div className={classes.dialog}>
+                    Danya
+                </div>
+                <div className={classes.dialog}>
+                    Ilya
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Yo</div>
+                <div className={classes.message}>Hi! Yo</div>
+            </div>
         </div>
     )
 }
