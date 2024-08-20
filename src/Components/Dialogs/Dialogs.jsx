08c292b4902189f3b5c2b6b01f42fdc4993ca1ba
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-20 23:16:52 +0500

    Добавил мапинг для сообщений

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -25,7 +25,8 @@ const Dialogs = (props) => {
         { id: 5, name: "Ilya" },
         { id: 6, name: "Vlad" }
     ]
-    let messagesData = [
+    let dialogsElements =  dialogsData.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />,)
+    let messages = [
         { id: 1, message: "Hi" },
         { id: 2, message: "Yo" },
         { id: 3, message: "Hi! Yo" },
@@ -33,17 +34,19 @@ const Dialogs = (props) => {
         { id: 5, message: "Hi! Yo" },
         { id: 6, message: "Hi! Yo" }
     ]
-    
+    let messagesElements = messages.map((m) => <Message message={m.message} />)
     
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[2].id} />
+                {
+                    dialogsElements
+                }
             </div>
             <div className={classes.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message} />
+                {
+                    messagesElements
+                }
             </div>
         </div>
     )
