430e143851efad991050f2a670ed7fccf5c38277
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-20 23:25:08 +0500

    Добавил маппинг в посты профиля

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,11 @@ import classes from "./MyPosts.module.css";
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  let postData = [
+  let posts = [
     { id: 1, message: "Hi, how are you?", likesCount: 12 },
     { id: 2, message: "It`s my first post", likesCount: 10},
 ]
+  let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
@@ -19,8 +20,7 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={classes.posts}>
-        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
-        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
+        {postsElements}
       </div>
     </div>)
 }
