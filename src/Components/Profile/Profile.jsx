986912bdfd4746eb3003977cc6bc4cc221bbbe67
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-25 17:02:03 +0500

    Прокинул пропс с постов в профиль

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -4,10 +4,14 @@ import classes from "./Profile.module.css";
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+  let posts = [
+    { id: 1, message: "Hi, how are you?", likesCount: 12 },
+    { id: 2, message: "It`s my first post", likesCount: 10},
+]
   return (
     <div>
       <ProfileInfo/>
-      <MyPosts />
+      <MyPosts posts = {posts}/>
     </div>)
 }
 export default Profile;
