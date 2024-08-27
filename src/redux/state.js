f459fccbb68871d1d04a57251321df8627ddfeb2
Сергей <S-E-R-G-I-O-14@yandex.ru>
2024-08-27 19:30:05 +0500

    Добавил диалогов

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,24 +1,32 @@
 let state = {
-    posts: [
-        { id: 1, message: "Hi, how are you?", likesCount: 12 },
-        { id: 2, message: "It`s my first post", likesCount: 10},
-      ],
-      dialogs: [
-        { id: 1, name: "Sergey" },
-        { id: 2, name: "Dimych" },
-        { id: 3, name: "Marina" },
-        { id: 4, name: "Artyom" },
-        { id: 5, name: "Ilya" },
-        { id: 6, name: "Vlad" }
-      ],
-      messages: [
-        { id: 1, message: "Hi" },
-        { id: 2, message: "Yo" },
-        { id: 3, message: "Hi! Yo" },
-        { id: 4, message: "Hi! Yo" },
-        { id: 5, message: "Hi! Yo" },
-        { id: 6, message: "Hi! Yo" }
-      ]
-  }
+    profilePage: {
+        posts: [
+            { id: 1, message: "Hi, how are you?", likesCount: 12 },
+            { id: 2, message: "It`s my first post", likesCount: 10 },
+        ]
 
-  export default state
+    },
+    dialogsPage: {
+        messages: [
+            { id: 1, message: "Hi" },
+            { id: 2, message: "Yo" },
+            { id: 3, message: "Hi! Yo" },
+            { id: 4, message: "Hi! Yo" },
+            { id: 5, message: "Hi! Yo" },
+            { id: 6, message: "Hi! Yo" },
+            { id: 7, message: "Hi! Yo" }
+        ],
+        dialogs: [
+            { id: 1, name: "Sergey" },
+            { id: 2, name: "Dimych" },
+            { id: 3, name: "Marina" },
+            { id: 4, name: "Artyom" },
+            { id: 5, name: "Ilya" },
+            { id: 6, name: "Vlad" },
+            { id: 7, name: "Ivan" }
+        ]
+    },
+    sidebar:{}
+}
+
+export default state
